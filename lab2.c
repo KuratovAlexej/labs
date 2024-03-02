a630c19a0c0eabed8c35a077713b35548c4abe72
KuratovAlexej <alexejkuratov93266@mail.ru>
2024-03-02 21:20:31 +1000

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -1,42 +1,36 @@
 #include <stdio.h> 
  
-int main() { 
-  double matrix1[3][3]; 
+int main() {
+  int size;
+    printf("Vvedite razmernoct': ");
+    scanf("%d", &size);
+  double matrix1[size][size]; 
   double sum1 = 0, sum2 = 0;
-  printf("Vvedite matritsy 3x3:\n");
-  for (int i = 0; i < 3; i++) { 
-    for (int j = 0; j < 3; j++) {
-       
+  printf("Vvedite matritsy:\n");
+  for (int i = 0; i < size; i++) { 
+    for (int j = 0; j < size; j++) {
       scanf("%lf", &matrix1[i][j]); 
     } 
   } 
    
-  for (int i = 0; i < 3; i++) { 
+  for (int i = 0; i < size; i++) { 
     sum1 += matrix1[i][i]; // Сумма главной диагонали
-    sum2 += matrix1[i][2-i]; // Сумма побочной диагонали
+    sum2 += matrix1[i][size-i-1]; // Сумма побочной диагонали
   }
   printf("Summa glavnoy diagonali: %0.2lf\n", sum1); 
   printf("Summa pobochnoy diagonali: %0.2lf\n", sum2); 
-  int matrix2[2][2];
-  int result[2][2];
-  printf("Vvedite matritsy 2x2:\n");
-  for(int i=0; i<2; i++) {
-    for(int j=0; j<2; j++) {
-      scanf("%d", &matrix2[i][j]);
-    }
-  }
-
-  for(int i=0; i<2; i++) {
-    for(int j=0; j<2; j++) {
+  int result[size][size];
+  for(int i=0; i<size; i++) {
+    for(int j=0; j<size; j++) {
       result[i][j] = 0;
-      for (int k=0; k<2; k++) {
-        result[i][j] += matrix2[i][k] * matrix2[k][j];
+      for (int k=0; k<size; k++) {
+        result[i][j] += matrix1[i][k] * matrix1[k][j];
       }
     }
   }
   printf("Kvadrat matritsy:\n");
-  for(int i=0; i<2; i++) {
-    for(int j=0; j<2; j++) {
+  for(int i=0; i<size; i++) {
+    for(int j=0; j<size; j++) {
       printf("%d ", result[i][j]);
     }
     printf("\n");

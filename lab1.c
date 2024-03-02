e3a1e832e5c99f86427622e47d1ebbd66dd3bf05
KuratovAlexej <alexejkuratov93266@mail.ru>
2024-03-02 16:19:56 +1000

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -8,12 +8,12 @@ while(1)
 { 
 int a,b,c,D; 
 double x1, x2; 
-printf("Введите коэффициенты: "); 
+printf("Vvedite coefficienty: "); 
 scanf("%d%d%d", &a,&b,&c); 
 D = b*b-4*a*c; 
-printf("Дискриминант:%d\n", D); 
+printf("Discriminant:%d\n", D); 
 if (D < 0) { 
-printf("Корней нет"); 
+printf("Korney net"); 
 } 
 if (D == 0) { 
 x1= (-b/(2*a)); 
@@ -25,7 +25,7 @@ x2=((-b-sqrt(D))/(2*a));
 printf("x1=%.2lf\nx2=%.2lf",x1,x2); 
 } 
  
- printf("\nПродолжить? (y/n): "); 
+ printf("\nContinue? (y/n): "); 
  scanf(" %c",&ch); 
  if (ch == 'n') break; 
 } 

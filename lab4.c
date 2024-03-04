8695ac63802641868603c8ba6ef0050675365125
KuratovAlexej <alexejkuratov93266@mail.ru>
2024-03-04 11:16:12 +1000

    Create lab4.c

---
+++ lab4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+double perimeter(double a, double b, double c) {
+    return a + b + c;
+}
+double square(double a, double b, double c) {
+    double s = perimeter(a, b, c) / 2;
+    return sqrt(s * (s - a) * (s - b) * (s - c));
+}

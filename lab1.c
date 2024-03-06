fd85f4a6a6dcb5641dc260304933d71b1693581b
KuratovAlexej <alexejkuratov93266@mail.ru>
2024-03-06 21:01:32 +1000

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -1,33 +1,31 @@
 #include <stdio.h> 
 #include <math.h> 
  
-int main() 
-{ 
-char ch; 
-while(1) 
-{ 
-int a,b,c,D; 
-double x1, x2; 
-printf("Vvedite coefficienty: "); 
-scanf("%d%d%d", &a,&b,&c); 
-D = b*b-4*a*c; 
-printf("Discriminant:%d\n", D); 
-if (D < 0) { 
-printf("Korney net"); 
-} 
-if (D == 0) { 
-x1= (-b/(2*a)); 
-printf("x1 =%.2lf",x1); 
-} 
-if (D > 0) { 
-x1=((-b+sqrt(D))/(2*a)); 
-x2=((-b-sqrt(D))/(2*a)); 
-printf("x1=%.2lf\nx2=%.2lf",x1,x2); 
-} 
+int main() { 
+    char ch; 
+    while(1) { 
+        int a,b,c,D; 
+        double x1, x2; 
+        printf("Vvedite coefficienty: "); 
+        scanf("%d%d%d", &a,&b,&c); 
+        D = b*b-4*a*c; 
+        printf("Discriminant:%d\n", D); 
+        if (D < 0) { 
+        printf("Korney net"); 
+        } 
+        if (D == 0) { 
+            x1= (-b/(2*a)); 
+            printf("x1 =%.2lf",x1); 
+        } 
+        if (D > 0) { 
+            x1=((-b+sqrt(D))/(2*a)); 
+            x2=((-b-sqrt(D))/(2*a)); 
+            printf("x1=%.2lf\nx2=%.2lf",x1,x2); 
+        } 
  
- printf("\nContinue? (y/n): "); 
- scanf(" %c",&ch); 
- if (ch == 'n') break; 
-} 
-return 0; 
+        printf("\nContinue? (y/n): "); 
+        scanf(" %c",&ch); 
+        if (ch == 'n') break; 
+    } 
+    return 0; 
 }

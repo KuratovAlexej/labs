182c6e5ef9a02779d9865a4b1ebcc3a6eda35d3b
KuratovAlexej <alexejkuratov93266@mail.ru>
2024-03-12 23:18:30 +1000

    Update lab5.c

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -1,2 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "triangle.h"
+void sum(int n, double m[n][n], double m1[n][n]); 
+void minus(int n, double m[n][n], double m1[n][n]); 
+void impr(int n, double m[n][n], double m1[n][n]);
+int main() { 
+    int n, i, j; 
+    char st; 
+ 
+    printf("size: "); 
+    scanf("%d", &n); 
+    double m[n][n], m1[n][n]; 
+ 
+    printf("Vvedite elementy pervoy matritsy:\n"); 
+    for (i = 0; i < n; i++) 
+        for (j = 0; j < n; j++) 
+            scanf("%lf", &m[i][j]); 
+ 
+    printf("Vvedite elementy vtoroy matritsy:\n"); 
+    for (i = 0; i < n; i++) 
+        for (j = 0; j < n; j++) 
+            scanf("%lf", &m1[i][j]); 
+ 
+    printf("\nVvedite operatsiyu('+','-','*'): "); 
+    scanf(" %c", &st);
+ 
+    if (st == '+') { 
+        sum(n, m, m1); 
+    } else if (st == '-') { 
+        minus(n, m, m1); 
+    } else if (st == '*') { 
+        impr(n, m, m1); 
+    } else { 
+        printf("Nevernaya operatsiya.\n"); 
+    } 
+ 
+    return 0; 
+} 

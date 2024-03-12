bcc32a9df02c5616607a6655accc93ea50dd59d1
KuratovAlexej <alexejkuratov93266@mail.ru>
2024-03-12 23:22:16 +1000

    Update triangle.h

--- lab5/triangle.h
+++ lab5/triangle.h
@@ -15,3 +15,17 @@ void minus(int n, double m[n][n], double m1[n][n]) {
         printf("\n"); 
     } 
 } 
+void impr(int n, double m[n][n], double m1[n][n]) { 
+    double result[n][n]; 
+    for (int i = 0; i < n; i++) { 
+        for (int j = 0; j < n; j++) { 
+            result[i][j] = 0;  
+            for (int k = 0; k < n; k++) { 
+                
+                result[i][j] += m[i][k] * m1[k][j]; 
+            } 
+            printf("%.1lf ", result[i][j]); 
+        } 
+        printf("\n"); 
+    } 
+}

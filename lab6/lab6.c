98a18007799f6d4483c5b4a52322ea7eb4d74c4b
KuratovAlexej <alexejkuratov93266@mail.ru>
2024-03-13 12:23:23 +1000

    Update lab6.c

--- lab6/lab6.c
+++ lab6/lab6.c
@@ -6,5 +6,14 @@ int main() {
     int year;
     opFile1 = fopen("t.txt", "r");// Открываем файл для чтения
     opFile2 = fopen("t1.txt", "w");// Открываем файл для записи отфильтрованных данных
+    while (fgets(line, sizeof(line), opFile1)) {
+        sscanf(line, "%*s %*s %*s %d", &year);
+        if (year > 1980) {
+            fputs(line, opFile2);
+        }
+    }
+    fclose(opFile1);
+    fclose(opFile2);
+    printf("Sortirovka zavershena, dannyye zapisany v fayl t1.txt.\n");
     return 0;
 }

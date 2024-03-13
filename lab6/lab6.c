3534b58b42024fc442a5f5d69c38618ba946e864
KuratovAlexej <alexejkuratov93266@mail.ru>
2024-03-13 12:13:50 +1000

    Update lab6.c

--- lab6/lab6.c
+++ lab6/lab6.c
@@ -4,5 +4,7 @@ int main() {
     FILE *opFile1, *opFile2;
     char line[100];
     int year;
+    opFile1 = fopen("t.txt", "r");// Открываем файл для чтения
+    opFile2 = fopen("t1.txt", "w");// Открываем файл для записи отфильтрованных данных
     return 0;
 }

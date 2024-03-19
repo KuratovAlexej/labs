be8dc1136fb8a14118966da3a9c1299b88df6778
KuratovAlexej <alexejkuratov93266@mail.ru>
2024-03-19 23:21:51 +1000

    Update triangle.h

--- lab5/triangle.h
+++ lab5/triangle.h
@@ -18,5 +18,20 @@ double* opermatrix(double** matrix1, double** matrix2, int n, char st) {
                 }
             }
             break;
+        case '*':
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    result[i * n + j] = 0;
+                    for (int k = 0; k < n; k++) {
+                        result[i * n + j] += matrix1[i][k] * matrix2[k][j];
+                    }
+                }
+            }
+            break;
+        default:
+            printf("Nevernaya operatsiya.\n");
+            free(result);
+            return NULL;
+    }
     return result;
 }

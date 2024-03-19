cffd2500f50a3aaff7f80599c0e30e5604095ed4
KuratovAlexej <alexejkuratov93266@mail.ru>
2024-03-19 23:16:45 +1000

    Update triangle.h

--- lab5/triangle.h
+++ lab5/triangle.h
@@ -1,31 +1,5 @@
-void sum(int n, double m[n][n], double m1[n][n]) { 
-    for (int i = 0; i < n; i++) { 
-        for (int j = 0; j < n; j++) { 
-            printf("%.1lf ", m[i][j] + m1[i][j]); 
-        } 
-        printf("\n"); 
-    } 
-} 
- 
-void minus(int n, double m[n][n], double m1[n][n]) { 
-    for (int i = 0; i < n; i++) { 
-        for (int j = 0; j < n; j++) { 
-            printf("%.1lf ", m[i][j] - m1[i][j]); 
-        } 
-        printf("\n"); 
-    } 
-} 
-void impr(int n, double m[n][n], double m1[n][n]) { 
-    double result[n][n]; 
-    for (int i = 0; i < n; i++) { 
-        for (int j = 0; j < n; j++) { 
-            result[i][j] = 0;  
-            for (int k = 0; k < n; k++) { 
-                
-                result[i][j] += m[i][k] * m1[k][j]; 
-            } 
-            printf("%.1lf ", result[i][j]); 
-        } 
-        printf("\n"); 
-    } 
-}
+#include <stdio.h>
+#include <stdlib.h>
+
+double* opermatrix(double** matrix1, double** matrix2, int n, char st) {
+    double* result = (double*)malloc(n * n * sizeof(double));

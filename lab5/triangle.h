91b982cc100b7a45b3fade6ac28878f3d7586e0d
KuratovAlexej <alexejkuratov93266@mail.ru>
2024-03-19 23:20:24 +1000

    Update triangle.h

--- lab5/triangle.h
+++ lab5/triangle.h
@@ -3,3 +3,20 @@
 
 double* opermatrix(double** matrix1, double** matrix2, int n, char st) {
     double* result = (double*)malloc(n * n * sizeof(double));
+    switch (st) {
+        case '+':
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    result[i * n + j] = matrix1[i][j] + matrix2[i][j];
+                }
+            }
+            break;
+        case '-':
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    result[i * n + j] = matrix1[i][j] - matrix2[i][j];
+                }
+            }
+            break;
+    return result;
+}

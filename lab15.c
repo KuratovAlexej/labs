303b3e623929886b99d816ff15965f5e95873e99
KuratovAlexej <alexejkuratov93266@mail.ru>
2024-03-26 23:29:45 +1000

    Update lab15.c

--- lab15.c
+++ lab15.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+void pr(int n) {
+    if ( n < 10) {
+        printf("%d ", n);
+    } else {
+        pr(n / 10);
+        printf("%d ", n % 10);
+    }
+}
 void obr(int n) { 
     printf("%d ", n % 10); 
     if (n >= 10) { 
@@ -8,7 +16,9 @@ void obr(int n) {
  
 int main() { 
     int N; 
-    scanf("%d", &N); 
+    scanf("%d", &N);
+    pr(N);
+    printf("\n");
     obr(N); 
     printf("\n"); 
     return 0; 

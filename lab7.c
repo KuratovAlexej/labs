f567a6a2a7350bc60b7e3a7d7f2cbc795449d60f
KuratovAlexej <alexejkuratov93266@mail.ru>
2024-04-06 23:22:13 +1000

    Update lab7.c

--- lab7.c
+++ lab7.c
@@ -11,3 +11,22 @@ void inputMass(humen mass[], int n){
         scanf("%s %s %d", mass[i].name, mass[i].surname, &mass[i].year);
     }
 }
+void copyMass(humen m1[], humen m2[], int n){
+    for(int i = 0; i < n; i++){
+        m2[i]=m1[i]; 
+    } 
+}
+void sortYear(humen mass[], int n){
+    for(int i =0; i < n; i++){
+        for(int j = 0; j < n - 1; j++){
+            if (mass[j].year > mass[j + 1].year){
+                humen temp = mass[j];
+                mass[j]= mass[j + 1];
+                mass[j + 1] = temp;
+            }
+        }
+    }
+
+
+
+}

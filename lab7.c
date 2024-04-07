fa99b19a630ed6d95f71584ec488a663464eaa79
KuratovAlexej <alexejkuratov93266@mail.ru>
2024-04-07 23:26:22 +1000

    Update lab7.c

--- lab7.c
+++ lab7.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 typedef struct{
-    char name[50];
-    char surname[50];
+    char name[20];
+    char surname[20];
     int year;
 } humen;
 void inputMass(humen mass[], int n){
     for(int i = 0; i < n; i++){
-        printf("Введите имя, фамилию и год рождения для человека %d:\n", i+1);
+        printf("Vvedite imya, familiyu i god rozhdeniya dlya cheloveka %d:\n", i+1);
         scanf("%s %s %d", mass[i].name, mass[i].surname, &mass[i].year);
     }
 }
 void copyMass(humen m1[], humen m2[], int n){
     for(int i = 0; i < n; i++){
-        m2[i]=m1[i]; 
+        m2[i] = m1[i]; 
     } 
 }
 void sortYear(humen mass[], int n){
@@ -26,7 +26,19 @@ void sortYear(humen mass[], int n){
             }
         }
     }
+}
+void printMass(const humen mass[], int n){
+    for(int i = 0; i < 4; i++){
+        printf("%s %s %d\n", mass[i].name, mass[i].surname, mass[i].year);
+    }
+}
+int main(){
+    humen mass1[4], mass2[4];
+    inputMass(mass1,4);
+    copyMass(mass1, mass2, 4);
+    sortYear(mass2,4);
+    printf("Otsortirovannyy massiv:\n"); 
+    printMass(mass2, 4); 
 
-
-
+    return 0;
 }

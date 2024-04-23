bcbdca81d732ae94e324e3af7ba61a6b7fe6d7ef
KuratovAlexej <alexejkuratov93266@mail.ru>
2024-04-23 23:49:01 +1000

    Update lab9.c

--- lab9.c
+++ lab9.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+double calculator(char* vyrazhenie){
+    char* token;
+    char* ntoken = NULL;
+    double otvet = 0;
+    token = strtok(vyrazhenie, "");
+}
 int main(){
     return 0;
 }

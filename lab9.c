c4bb3fd6fda4fd46487782009d7e8bed3ab62a45
KuratovAlexej <alexejkuratov93266@mail.ru>
2024-05-15 11:36:30 +1000

    Update lab9.c

--- lab9.c
+++ lab9.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+void space(char *phrase, char *result) { 
+    for (int i = 0; phrase[i] != '\0'; i++) { 
+        if (phrase[i] == '+' || phrase[i] == '-') { 
+            if(i > 0 && phrase[i-1] != ' ') 
+                *result++ = ' '; 
+            *result++ = phrase[i]; 
+            if(phrase[i+1] != ' ') 
+                *result++ = ' '; 
+        } else { 
+            *result++ = phrase[i]; 
+        } 
+    } 
+    *result = '\0';  
+} 
 double calculator(char* vyrazhenie){
     char* token;
-    char* ntoken = NULL;
     double otvet = 0;
-    token = strtok(vyrazhenie, "");
+    token = strtok(vyrazhenie, " ");
     while (token != NULL) {
          if (strcmp(token, "+") == 0) {
             token = strtok(NULL, " ");
@@ -22,10 +35,15 @@ double calculator(char* vyrazhenie){
 }
 int main(){
     char phrase[100];
+    char result_phrase[200]; // для выражения с пробелами
     printf("Vvedite vyrazheniye: ");
     fgets(phrase, 100, stdin);
     phrase[strlen(phrase) - 1] = '\0';
-    double result = calculator(phrase);
+    space(phrase, result_phrase); // добавляем пробелы
+    printf("Iskhodnoye: %s\n", phrase);
+    printf("C probelami: %s\n", result_phrase);
+    double result = calculator(result_phrase);
     printf("Result: %.2lf\n", result);
     return 0;
 }
+

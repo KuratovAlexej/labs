9a836995590b14d779a26a33f6e32131c47398cb
KuratovAlexej <alexejkuratov93266@mail.ru>
2024-06-03 19:13:07 +1000

    Update lab10.c

--- lab10.c
+++ lab10.c
@@ -1,4 +1,41 @@
-#include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
-#define Max(mas, n)
+
+#define MAX(arr, n, max) {\
+  max = arr[0];\
+    for (int i = 1; i < n; ++i) {\
+        if (arr[i] > max) {\
+            max = arr[i];\
+        }\
+    }\
+}
+
+#define AVR(arr, n, avr) { \
+    double sum = 0; \
+    for (int i = 0; i < n; i++) { \
+        sum += arr[i]; \
+    } \
+    avr = sum / n; \
+}
+
+int main() {
+    int n;
+    printf("Введите количество чисел: ");
+    scanf("%d", &n);
+
+    double numbers[n];
+    double max_num;
+    double avr;
+
+    printf("Введите %d чисел через пробел: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%lf", &numbers[i]);
+    }
+
+    MAX(numbers, n, max_num);
+    AVR(numbers, n, avr);
+
+    printf("Максимальное число: %.2f\n", max_num);
+    printf("Среднее арифметическое число: %.2f\n", avr);
+
+    return 0;
+}
